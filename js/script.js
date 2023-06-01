5ef65681b9f6d610cf6d3844947bb4ba511b31ea
hrdisendoorn <133860884+hrdisendoorn@users.noreply.github.com>
2023-06-01 19:01:21 +0200

    Batch update

---
+++ js/script.js
@@ -0,0 +1,12 @@
+
+
+    window.silex = window.silex || {}
+    window.silex.data = {"site":{"width":960},"pages":[{"id":"page-design","displayName":"Design","link":{"linkType":"LinkTypePage","href":"#!page-design"},"canDelete":true,"canProperties":true,"canMove":true,"canRename":true,"opened":false},{"id":"page-var-1","displayName":"Var 1","link":{"linkType":"LinkTypePage","href":"#!page-var-1"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true,"opened":false},{"id":"page-var-2","displayName":"Var 2","link":{"linkType":"LinkTypePage","href":"#!page-var-2"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true,"opened":false},{"id":"page-var-3","displayName":"Var 3","link":{"linkType":"LinkTypePage","href":"#!page-var-3"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true,"opened":false},{"id":"page-proc","displayName":"Proc","link":{"linkType":"LinkTypePage","href":"#!page-proc"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true,"opened":false},{"id":"page-missievisie","displayName":"missievisie","link":{"linkType":"LinkTypePage","href":"#!page-missievisie"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true,"opened":false},{"id":"page-watisnodig","displayName":"Watisnodig","link":{"linkType":"LinkTypePage","href":"#!page-watisnodig"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true,"opened":false},{"id":"page-opties","displayName":"Opties","link":{"linkType":"LinkTypePage","href":"#!page-opties"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true,"opened":false},{"id":"page-watradenweaan","displayName":"Watradenweaan","link":{"linkType":"LinkTypePage","href":"#!page-watradenweaan"},"canDelete":true,"canRename":true,"canMove":true,"canProperties":true,"opened":false}]}
+
+$(('paged-element').onload = function(){
+  $(window).scrollTop(0);
+});
+
+$(('window').onload = function(){
+  $(window).scrollTop(0);
+});
